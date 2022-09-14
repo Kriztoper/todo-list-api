ff93e3eaa6abef337e5c4ccde2e994e284db964a
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-14 23:29:04 +0800

    feature,api-endpoint: add endpoint reorder, cover scenario 1 move task downward

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,4 +34,50 @@ class TasksController < ApplicationController
     render json: tasks, except: [:created_at, :updated_at]
   end
 
+  # Scenario 1: small order_number task is moved to bigger order_number at the middle
+  # given tasks:
+  # [
+  #     {
+  #         "id": 1,
+  #         "content": "Buy grocery",
+  #         "order_number": 1
+  #     },
+  #     {
+  #         "id": 2,
+  #         "content": "Pay bills",
+  #         "order_number": 2
+  #     },
+  #     {
+  #         "id": 3,
+  #         "content": "Laundry dirty clothes and bed sheet",
+  #         "order_number": 3
+  #     }
+  # ]
+  # then we want to move 1st task (task with order_number = 1) as 2nd task (task with order_number = 2)
+  # Proposed solution: Since task to move is from smaller order_number then moved to bigger order_number,
+  # the order_numbers below the target_order_number will be updated and reduced by 1, then the task to move's
+  # order_number will be set as the target_order_number
+  def reorder
+    task_to_move = Task.find(params[:this_id])
+    target_order_number = params[:target_order_number].to_i
+    if task_to_move.order_number < target_order_number
+      move_up_tasks_above_target task_to_move, target_order_number
+    end
+    render json: task_to_move
+  end
+
+  private
+
+  def move_up_tasks_above_target(task_to_move, target_order_number)
+    from_range = task_to_move.order_number + 1
+    to_range = target_order_number
+    (from_range..to_range).each do |order_number|
+      current_task = Task.where(order_number: order_number).first
+      unless current_task.nil?
+        current_task.update_column(:order_number, current_task.order_number - 1)
+      end
+    end
+    task_to_move.update(order_number: target_order_number)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   scope :api do
     scope :v1 do
-      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      get '/tasks/reorder/:this_id/to/:target_order_number', to: 'tasks#reorder'
+      resources :tasks
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
