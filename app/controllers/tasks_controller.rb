67aa4286439338e934a1defd9a122883215507d9
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-14 22:08:29 +0800

    refactor: change instance variables to local variables

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,37 +1,37 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
-    render json: @tasks
+    tasks = Task.all
+    render json: tasks
   end
 
   def show
-    @task = Task.find(params[:id])
-    render json: @task
+    task = Task.find(params[:id])
+    render json: task
   end
 
   def create
-    @new_task = Task.create(
+    new_task = Task.create(
       content: params[:content],
       order_number: params[:order_number]
     )
-    render json: @new_task
+    render json: new_task
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(
+    task = Task.find(params[:id])
+    task.update(
       content: params[:content],
       order_number: params[:order_number]
     )
-    render json: @task
+    render json: task
   end
 
   def destroy
-    @tasks = Task.all
-    @deleted_task = Task.find(params[:id])
-    @deleted_task.destroy
-    render json: @tasks
+    tasks = Task.all
+    deleted_task = Task.find(params[:id])
+    deleted_task.destroy
+    render json: tasks
   end
 
 end
