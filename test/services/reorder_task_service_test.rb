360b1217622da46fc7ef726240bb0eb4ca57ae9e
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-15 21:59:08 +0800

    refactor: apply rubocop enhancement suggestions

--- test/services/reorder_task_service_test.rb
+++ test/services/reorder_task_service_test.rb
@@ -1,21 +1,21 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ReorderTaskServiceTest < ActiveSupport::TestCase
-
-  test "task to move with order number originally at 1 then is moved to order number 3" do
-    task_to_move = Task.new :content => 'Buy grocery', :order_number => 1
+  test 'task to move with order number originally at 1 then is moved to order number 3' do
+    task_to_move = Task.new content: 'Buy grocery', order_number: 1
     target_order_number = 3
     service = ReorderTaskService.new
     service.move_up_tasks_above_target task_to_move, target_order_number
     assert_equal 3, task_to_move.order_number
   end
 
-  test "task to move with order number originally at 3 then is moved to order number 1" do
-    task_to_move = Task.new :content => 'Buy grocery', :order_number => 3
+  test 'task to move with order number originally at 3 then is moved to order number 1' do
+    task_to_move = Task.new content: 'Buy grocery', order_number: 3
     target_order_number = 1
     service = ReorderTaskService.new
     service.move_up_tasks_above_target task_to_move, target_order_number
     assert_equal 1, task_to_move.order_number
   end
-
 end
