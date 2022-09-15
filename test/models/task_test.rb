6edb7041993da440fbbec78bc809cd2c154df86b
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-15 20:32:36 +0800

    test: create new simple test for asserting content of newly created task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,10 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "create new task to buy grocery then created task should be buy grocery" do
+    new_task = Task.new :content => 'Buy grocery', :order_number => 1
+    assert_equal 'Buy grocery', new_task.content
+  end
+
 end
